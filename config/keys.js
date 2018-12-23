c55cdbda9230941905146075d934027dd62e276f
Omerenma <godwin2341@gmail.com>
2018-12-23 02:36:40 -0800

    Initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,10 @@
-module.export ={
-  mongoURI:'mongodb://kingsly8:kingsly8@ds225902.mlab.com:25902/developersconnect'
+ module.exports ={
+  mongoURI:'mongodb://kingsly8:kingsly8@ds225902.mlab.com:25902/developersconnect',
+  secretOrkey:'secret'
+
 }
+
+
+// const mongoURI = 'mongodb://kingsly8:kingsly8@ds225902.mlab.com:25902/developersconnect';
+// const secretOrkey = 'secret'
+//module.exports = mongoURI;
--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 // Require all dependencies
-var express = require("express");
+const express = require("express");
 var app = express();
-const mongoose = require('mongoose')
+const bodyPaser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport')
+const jwt = require('jsonwebtoken');
+module.exports = jwt;
+// Require route files
 const users = require('./routes/api/users');
 const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
+
+//Body parser middleware
+app.use(bodyPaser.urlencoded({extended:false}));
+app.use(bodyPaser.json());
+
 // DB config
-const Uri ='mongodb://kingsly8:kingsly8@ds225902.mlab.com:25902/developersconnect'
-//const db = require('./config/keys')
-//connect to mongodb
-mongoose.connect(Uri)
+//const uri = 'mongodb://kingsly8:kingsly8@ds225902.mlab.com:25902/developersconnect'
+//const db = require('./config/keys').mongoURI;
+//connect to mongodb 
+mongoose.connect('mongodb://localhost:27017/devconnect')
     .then(() => console.log('MongoDb connect'))
     .catch(err => console.log(err));
-
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/profiles', profiles);
-app.use('/api/posts', posts);
+//app.use('/api/posts', posts);
 
 // Create Server
 var port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server app running on port ${port}`));
+
